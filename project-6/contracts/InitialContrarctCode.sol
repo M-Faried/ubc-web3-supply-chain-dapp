16bef9c831e45143c9f55321a6fbc8b9542d3981
M Faried <m.a.faried@gmail.com>
2022-06-20 13:30:16 +0200

    * Fixed the enum missing Shipped state. * Added the missing shipped event. * Formatted the code.

--- project-6/contracts/InitialContrarctCode.sol
+++ project-6/contracts/InitialContrarctCode.sol
@@ -2,32 +2,36 @@ pragma solidity ^0.4.24;
 
 // Define a contract 'Lemonade Stand'
 contract LemonadeStand {
-
     // Variable: Owner
     address owner;
 
     // Variable: SKU count
-    uint skuCount;
+    uint256 skuCount;
 
     // Event: 'State' with value 'ForSale'
-    enum State { ForSale, Sold }
+    enum State {
+        ForSale,
+        Sold,
+        Shipped
+    }
 
     // Struct: Item. name, sku, price, state, seller, buyer
     struct Item {
-        string  name;
-        uint  sku;
-        uint  price;
-        State  state;
-        address  seller;
-        address  buyer;
+        string name;
+        uint256 sku;
+        uint256 price;
+        State state;
+        address seller;
+        address buyer;
     }
 
     // Define a public mapping 'items' that maps the SKU (a number) to an Item.
-    mapping (uint => Item) items;
+    mapping(uint256 => Item) items;
 
     // Events
-    event ForSale(uint skuCount);
-    event Sold(uint sku);
+    event ForSale(uint256 skuCount);
+    event Sold(uint256 sku);
+    event Shipped(uint256 sku);
 
     // Modifier: Only Owner see if msg.sender == owner of the contract
     modifier onlyOwner() {
@@ -36,25 +40,25 @@ contract LemonadeStand {
     }
 
     // Define a modifier that verifies the Caller
-    modifier verifyCaller (address _address) {
+    modifier verifyCaller(address _address) {
         require(msg.sender == _address);
         _;
     }
 
     // Define a modifier that checks if the paid amount is sufficient to cover the price
-    modifier paidEnough(uint _price) {
+    modifier paidEnough(uint256 _price) {
         require(msg.value >= _price);
         _;
     }
 
     // Define a modifier that checks if an item.state of a sku is ForSale
-    modifier forSale(uint _sku) {
+    modifier forSale(uint256 _sku) {
         require(items[_sku].state == State.ForSale);
         _;
     }
 
     // Define a modifier that checks if an item.state of a sku is Sold
-    modifier sold(uint _sku) {
+    modifier sold(uint256 _sku) {
         require(items[_sku].state == State.Sold);
         _;
     }
@@ -64,7 +68,7 @@ contract LemonadeStand {
         skuCount = 0;
     }
 
-    function addItem(string _name, uint _price) onlyOwner public {
+    function addItem(string _name, uint256 _price) public onlyOwner {
         // Increment sku
         skuCount = skuCount + 1;
 
@@ -72,12 +76,24 @@ contract LemonadeStand {
         emit ForSale(skuCount);
 
         // Add the new item into inventory and mark it for sale
-        items[skuCount] = Item({name: _name, sku: skuCount, price: _price, state: State.ForSale, seller: msg.sender, buyer: 0});
+        items[skuCount] = Item({
+            name: _name,
+            sku: skuCount,
+            price: _price,
+            state: State.ForSale,
+            seller: msg.sender,
+            buyer: 0
+        });
     }
 
-    function buyItem(uint sku) forSale(sku) paidEnough(items[sku].price) public payable{
+    function buyItem(uint256 sku)
+        public
+        payable
+        forSale(sku)
+        paidEnough(items[sku].price)
+    {
         address buyer = msg.sender;
-        uint price = items[sku].price;
+        uint256 price = items[sku].price;
 
         // Update Buyer
         items[sku].buyer = buyer;
@@ -92,19 +108,34 @@ contract LemonadeStand {
         emit Sold(sku);
     }
 
-    function fetchItem(uint _sku) public view returns (string name, uint sku, uint price, string stateIs, address seller, address buyer) {
-        uint state;
+    function fetchItem(uint256 _sku)
+        public
+        view
+        returns (
+            string name,
+            uint256 sku,
+            uint256 price,
+            string stateIs,
+            address seller,
+            address buyer
+        )
+    {
+        uint256 state;
         name = items[_sku].name;
         sku = items[_sku].sku;
         price = items[_sku].price;
-        state = uint(items[_sku].state);
+        state = uint256(items[_sku].state);
 
-        if( state == 0) {
+        if (state == 0) {
             stateIs = "For Sale";
         }
 
-        if( state == 1) {
+        if (state == 1) {
             stateIs = "Sold";
+        }
+
+        if (state == 2) {
+            stateIs = "Shipped";
         }
 
         seller = items[_sku].seller;
@@ -112,12 +143,13 @@ contract LemonadeStand {
     }
 
     // Define a function 'shipItem' that allows the seller to change the state to 'Shipped'
-    function shipItem(uint sku) public
+    function shipItem(uint256 sku)
+        public
         // Call modifier to check if the item is sold
         sold(sku)
         // Call modifier to check if the invoker is seller
-        verifyCaller(items[sku].seller) {
-
+        verifyCaller(items[sku].seller)
+    {
         // Update state
         items[sku].state = State.Shipped;
         // Emit the appropriate event
@@ -125,31 +157,33 @@ contract LemonadeStand {
     }
 
     // Define a modifier that checks the price and refunds the remaining balance
-modifier checkValue(uint _sku) {
-    _;
-    uint _price = items[_sku].price;
-    uint amountToRefund = msg.value - _price;
-    items[_sku].buyer.transfer(amountToRefund);
-}
-
-// Define a function 'buyItem' that allows one to purchase an item from the inventory
-function buyItem(uint sku) public payable
-    // Call modifier to check if sku is for sale
-    forSale(sku)
-    // Call modifer to check if buyer has paid enough
-    paidEnough(items[sku].price)
-    // Call modifer to send any excess ether back to buyer
-    checkValue(sku) {
-    address buyer = msg.sender;
-    uint  price = items[sku].price;
-    // Update buyer
-    items[sku].buyer = buyer;
-    // Update state
-    items[sku].state = State.Sold;
-    // Transfer money to seller
-    items[sku].seller.transfer(price);
-    // emit the appropriate event
-    emit Sold(sku);
+    modifier checkValue(uint256 _sku) {
+        _;
+        uint256 _price = items[_sku].price;
+        uint256 amountToRefund = msg.value - _price;
+        items[_sku].buyer.transfer(amountToRefund);
     }
 
+    // Define a function 'buyItem' that allows one to purchase an item from the inventory
+    function buyItem(uint256 sku)
+        public
+        payable
+        // Call modifier to check if sku is for sale
+        forSale(sku)
+        // Call modifer to check if buyer has paid enough
+        paidEnough(items[sku].price)
+        // Call modifer to send any excess ether back to buyer
+        checkValue(sku)
+    {
+        address buyer = msg.sender;
+        uint256 price = items[sku].price;
+        // Update buyer
+        items[sku].buyer = buyer;
+        // Update state
+        items[sku].state = State.Sold;
+        // Transfer money to seller
+        items[sku].seller.transfer(price);
+        // emit the appropriate event
+        emit Sold(sku);
+    }
 }
